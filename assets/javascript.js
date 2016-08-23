a2bb1bc96f757be5c5d50b0ae3fef7030815a687
AmandineP <amandine.pean@free.fr>
2016-08-23 09:21:06 +0200

    menu burger

--- assets/javascript.js
+++ assets/javascript.js
@@ -2,3 +2,18 @@ $('button').click(function(e) {
   e.preventDefault();
   $('main').addClass('flip');
 });
+
+
+(function myFunction() {
+    var logoBurger = document.getElementById('logoBurger');
+
+    logoBurger.onclick = function () {
+
+        if (move.style.marginLeft === "500px") {
+            move.style.marginLeft = "0";
+
+        } else {
+            move.style.marginLeft = "500px";
+        }
+    };
+})();
